0077e00fca44fe9557a29cd53370d4d86aaefdb0
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-08-25 00:29:11 -0300

    Modificações em consumer.py consumindo as requisições de created, updated e deleted de product do django

--- consumer.py
+++ consumer.py
@@ -1,5 +1,7 @@
+import json
 import pika
 from config import CLOUDAMQP_URL
+from main import Product, db
 
 params = pika.URLParameters(CLOUDAMQP_URL)
 
@@ -9,9 +11,32 @@ channel = connection.channel()
 
 channel.queue_declare(queue='main')
 
+# Consome os dados vindo do app django
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'],
+                          image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
+
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
