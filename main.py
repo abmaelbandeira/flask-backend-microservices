16770933bea258a24dd62c98de410e050dcde295
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-08-25 00:40:01 -0300

    Adiciona notação @dataclass em models e finaliza rota api/products com jsonify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from dataclasses import dataclass
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy import UniqueConstraint
@@ -9,12 +10,19 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
+
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
 
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
@@ -23,9 +31,9 @@ class ProductUser(db.Model):
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return 'Hello'
+    return jsonify(Product.query.all())
 
 
 if __name__ == '__main__':
