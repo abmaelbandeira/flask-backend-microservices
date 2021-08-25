af6cbc4586cb41bf0c0657146e668bc016e4db3d
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-08-24 23:43:21 -0300

    Adiciona auto_ack=True em consumer.py

--- consumer.py
+++ consumer.py
@@ -13,7 +13,7 @@ def callback(ch, method, properties, body):
     print('Received in main')
     print(body)
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
